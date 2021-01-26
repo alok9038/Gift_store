e572fc2aedd41eb8b942b0229d0aae111770da18
alok9038 <kumaralok1884@gmail.com>
2021-01-26 11:08:49 +0530

    error update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -69,7 +69,7 @@ class UserController extends Controller
 
         if($id!= null){
             $query = Order::where('id',$id)->update([
-                'coupon_id' => 10
+                'coupon_id' => null
                 ]);
                 return redirect()->back();
         }
