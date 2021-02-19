83d46b7a62c561959e196ea2c28f3c8990e0b9a9
alok9038 <kumaralok1884@gmail.com>
2021-02-19 17:51:51 +0530

    bug

--- resources/views/layouts/homebase.blade.php
+++ resources/views/layouts/homebase.blade.php
@@ -13,9 +13,455 @@
         
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ url('css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <script src="{{ asset('js/font.js') }}" crossorigin="anonymous"></script>
     </head>
+    <style>
+        .menu-toggle {
+            position: fixed;
+            left: 15px;
+            top:3px;
+            padding-top: 5px;
+            width: 50px;
+            height: 50px;
+            text-align: center;
+            color:white;
+            transition: all linear .2s;
+            line-height: 50px;
+            z-index: 9999;
+        }
+        .side-toggle{
+            position: fixed;
+            right: 15px!important;
+            top:6px;
+            padding-top: 5px;
+            width: 50px;
+            height: 50px;
+            text-align: center;
+            color:white;
+            transition: all linear .2s;
+            line-height: 50px;
+            z-index: 9999;
+        }
+        .mobile-cart{
+            position: fixed;
+            right: 15px!important;
+            top:0;
+            width: 50px;
+            text-align: center;
+            color:white;
+            transition: all linear .2s;
+            line-height: 40px;
+            z-index: 9999;
+        }
+        .menu-toggle:focus,
+        .menu-toggle:hover {
+            color:white;
+        }
+        
+        .menu-toggle:hover {
+            background: #ff616f;
+        }
+
+        .cat .nav-link:hover{
+            color: #ff1744!important;
+        }
+        .cat .nav-link{
+            color: black!important;
+            font-weight: 500;
+        }
+        /* ************************ */
+        #sidebar-wrapper {
+            position: fixed;
+            z-index: 9999;
+            top: 0;
+            left: 0;
+            width: 250px;
+            height: 100%;
+            transition: all 0.5s ease 0s;
+            transform: translateX(-250px);
+            background: #1a1a2e;
+            border-left: 1px solid rgba(255, 255, 255, 0.1);
+        }
+        
+        .sidebar-nav {
+            position: absolute;
+            top: 0;
+            width: 250px;
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+        
+        .sidebar-nav li.sidebar-nav-item a {
+            display: block;
+            text-decoration: none;
+            color: #fff;
+            padding: 15px;
+        }
+        .sidebar-nav li a:hover {
+            text-decoration: none;
+            color: #fff;
+            background: rgba(255, 255, 255, 0.2);
+        }
+        
+        .sidebar-nav li a:active,
+        .sidebar-nav li a:focus {
+            text-decoration: none;
+        }
+        
+        .sidebar-nav > .sidebar-brand {
+            font-size: 1.2rem;
+            background: rgba(52, 58, 64, 0.1);
+            height: 80px;
+            text-decoration: none;
+            line-height: 50px;
+            padding-top: 15px;
+            padding-bottom: 15px;
+            padding-left: 15px;
+        }
+        .sidebar-nav > .sidebar-brand a {
+            color: #fff;
+        }
+        
+        .sidebar-nav > .sidebar-brand a:hover {
+            color: #fff;
+            background: none;
+        }
+        
+        #sidebar-wrapper.active {
+            left: 250px;
+            width: 250px;
+            transition: all 0.4s ease 0s;
+        }
+        .menu-toggle{
+            display:none;
+        }
+
+        .h-link{
+            transition: .2s linear all;
+        }
+        .h-link .nav-link{
+            color: white!important;
+        }
+        .h-link .nav-link:hover{
+            color: lightgrey!important;
+        }
+        .content{
+            margin-top: 170px;
+        }
+        .mobile-cart{
+            display: none;
+        }
+        .navbar-mobile{
+        display: none;
+        }
+        .desktop-navbar{
+        position:-webkit-sticky;position:sticky;top:0;z-index:9999;
+        }
+
+        @media (max-width: 992px) {
+                .desktop-nav, .desktop-search{
+                    display: none;
+                }
+                .content{
+                    margin-top: 120px;
+                }
+                .menu-toggle{
+                    display: block;
+                }
+                .desktop-navbar{
+                position: relative;
+                z-index:0;
+                }
+                .navbar-brand, .logo{
+                    margin-left: auto;
+                    margin-right: auto;
+                    width: calc(100% - 50%);
+                }
+                .home-img{
+                    background-size: cover!important;
+                    background-position:top 0px center!important;
+                    margin-top: 0px;;
+                    height: 290px;
+                    padding-bottom: 50px!important;
+                }
+                .navbar-mobile{
+                    box-shadow: 0 .125rem .25rem rgba(0,0,0,.075)!important;
+                    position: fixed!important;
+                    display:block;
+                    width: 100%;
+                }
+                .mobile-cart{
+                    display: block;
+                    z-index:9999;
+                }
+                .post-item .card-img-top{
+                    height: 330px!important;
+                    object-fit: cover!important;
+                }
+                
+                .post-item{
+                    width: 80%;
+                    margin-left: auto;
+                    margin-right: auto;
+                }
+                .footer{
+                margin-bottom:67px;
+                }
+        }
+
+        @media (max-width:1300){
+            .sidebar-wrapper{
+                display: none;
+            }
+            .home-img{
+                background-size: cover;
+                background-position:bottom 0px center!important;
+            
+            }
+            
+        }
+        @media (max-width:1100){
+            
+            .home-img{
+                background-size: cover;
+                height: auto;
+            
+            }
+            
+        }
+
+        .home-img{
+            
+            /* background-attachment:fixed;  */
+            background-size:100%; 
+            /* background-position:top 63px center; */
+            background-repeat:no-repeat;
+            height: 100%;
+        }
+
+        .sticky {
+            position: fixed;
+            top: 0;
+            width: 100%;
+        }
+        
+        .sticky + .content {
+            padding-top: 60px;
+        }
+        .post-item{
+            transition: .3s all linear;
+            box-shadow: 0 .1rem 0.5rem rgba(0,0,0,.45)!important;
+        }
+        .post-item:hover, .post-item:active{
+            box-shadow: 0 .8rem 1rem rgba(0,0,0,.55)!important;
+        }
+        .overlay{
+            position: absolute;
+            bottom: 0;
+            color: white;
+            padding: 20px;
+            width: 99.4%;
+            background-color: rgba(0, 0, 0, 0.61);
+        }
+        
+
+
+
+        /* .category-slider */
+
+        /* Slider */
+
+        .slick-slide {
+            margin: 0px 20px;
+        }
+
+        .slick-slide img {
+            width: 100%;
+        }
+
+        .slick-slider
+        {
+            position: relative;
+            display: block;
+            box-sizing: border-box;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+                    user-select: none;
+            -webkit-touch-callout: none;
+            -khtml-user-select: none;
+            -ms-touch-action: pan-y;
+                touch-action: pan-y;
+            -webkit-tap-highlight-color: transparent;
+        }
+
+        .slick-list
+        {
+            position: relative;
+            display: block;
+            overflow: hidden;
+            margin: 0;
+            padding: 0;
+        }
+        .slick-list:focus
+        {
+            outline: none;
+        }
+        .slick-list.dragging
+        {
+            cursor: pointer;
+            cursor: hand;
+        }
+
+        .slick-slider .slick-track,
+        .slick-slider .slick-list
+        {
+            -webkit-transform: translate3d(0, 0, 0);
+            -moz-transform: translate3d(0, 0, 0);
+                -ms-transform: translate3d(0, 0, 0);
+                -o-transform: translate3d(0, 0, 0);
+                    transform: translate3d(0, 0, 0);
+        }
+
+        .slick-track
+        {
+            position: relative;
+            top: 0;
+            left: 0;
+            display: block;
+        }
+        .slick-track:before,
+        .slick-track:after
+        {
+            display: table;
+            content: '';
+        }
+        .slick-track:after
+        {
+            clear: both;
+        }
+        .slick-loading .slick-track
+        {
+            visibility: hidden;
+        }
+
+        .slick-slide
+        {
+            display: none;
+            float: left;
+            height: 100%;
+            min-height: 1px;
+        }
+        [dir='rtl'] .slick-slide
+        {
+            float: right;
+        }
+        .slick-slide img
+        {
+            display: block;
+        }
+        .slick-slide.slick-loading img
+        {
+            display: none;
+        }
+        .slick-slide.dragging img
+        {
+            pointer-events: none;
+        }
+        .slick-initialized .slick-slide
+        {
+            display: block;
+        }
+        .slick-loading .slick-slide
+        {
+            visibility: hidden;
+        }
+        .slick-vertical .slick-slide
+        {
+            display: block;
+            height: auto;
+            border: 1px solid transparent;
+        }
+        .slick-arrow.slick-hidden {
+            display: none;
+        }
+        .paginator {
+        margin-bottom: 30px;
+        }
+        .prev, .next{
+        color:grey;
+        }
+        .prev:hover, .next:hover{
+        color:#ff1744;
+        cursor: pointer;
+        }
+        .slider-wrapper {
+        width: auto;
+        }
+
+        .slider-for__item img {
+        width: 100%;
+        }
+
+        .slider-nav__item {
+        height: 90px;
+        cursor: pointer;
+        }
+
+        /* custom theme color */
+
+        .bg-theme{
+            background-color:#ff1744;
+        }
+        .text-theme{
+            color: #ff1744;
+        }
+        .bg-green{
+            background-color: #00c853;
+        }
+        .mobile-foot-menu{
+            -webkit-box-shadow: 0px -4px 3px rgba(50, 50, 50, 0.75);
+            -moz-box-shadow: 0px -4px 3px rgba(50, 50, 50, 0.75);
+            box-shadow: 0px -0.1rem 0.5rem rgba(50, 50, 50, 0.781);
+        }
+        /* social icon */
+
+        .social-icon ul{
+            padding:0;
+        }
+        .social-icon ul li{
+            display: inline;
+        }
+        .fa-facebook, .fa-instagram, .fa-twitter, .fa-youtube{
+            color: white;
+            padding: 0 14px;
+            height: 40px;
+            padding-top: 12px;
+            font-size: 1.1rem;
+            border-radius: 12px;
+            background-color: #102027;
+            transition: all .3s linear;
+        }
+        .fa-facebook:hover{
+            
+            background-color: #3b5998;
+        }
+        .fa-instagram:hover{
+            background:linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d);
+        }
+        .fa-twitter:hover{
+            background-color:#00acee ;
+        }
+        .fa-youtube:hover{
+            background-color: #FF0000;
+        }
+        .ks-text{
+        color: rgb(226, 226, 226);
+        font-size: 1.05rem;
+        }
+    </style>
     <body>
         <div class="header">
             <a class="menu-toggle rounded d-lg-none d-block" href="#">
@@ -201,7 +647,65 @@
             });
         </script>
         {{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> --}}
-        <script src="{{ url('js/main.js') }}"></script>
+        <script src="{{ asset('js/main.js') }}"></script>
+
+        {{-- try --}}
+        <script>
+            (function ($) {
+                ("use strict");
+
+                // closes the sidebar menu
+                $(".menu-toggle, .side-toggle").click(function (e) {
+                e.preventDefault();
+                $("#sidebar-wrapper").toggleClass("active");
+                $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times, .side-toggle > .fa-bars, .side-toggle > .fa-bars").toggleClass(
+                "fa-bars fa-times"
+                );
+                $(this).toggleClass("active");
+                });
+
+                //smooth scrolling using jquery and easing
+                $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+                if (
+                location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+                location.hostname == this.hostname
+                ) {
+                var target = $(this.hash);
+                target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+                if (target.length) {
+                $("html, body").animate(
+                    {
+                    scrollTop: target.offset().top,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+                }
+                }
+                });
+                $("#sidebar-wrapper .js-scroll-trigger").click(function () {
+                $("#sidebar-wrapper").removeClass("active");
+                $(".menu-toggle, .side-toggle").removeClass("active");
+                $(".menu-toggle > fa.bars, .menu-toggle > .fa-times, .side-toggle > fa-times, .side-toggle > .fa-times").toggleClass(
+                "fa-bars fa-times"
+                );
+                });
+
+                $(document).scroll(function () {
+                var scrollDistance = $(this).scrollTop();
+                if (scrollDistance > 100) {
+                $(".scroll-to-top").fadeIn();
+                } else {
+                $(".scroll-to-top").fadeOut();
+                }
+                });
+                })(jQuery);
+
+        </script>
+        {{-- try end --}}
+
         <script>
             var lastScrollTop = 0, px = 0, scrollDownBool = false;
             $(window).scroll(function(event){
